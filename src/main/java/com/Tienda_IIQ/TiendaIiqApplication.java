df7fd77a7acd9e110d7f5233eb5935b2ac6988fa
AdrianCampos21 <106779974+AdrianCampos21@users.noreply.github.com>
2023-05-18 20:09:07 -0600

    Primer Commit del Proyecto Tienda

---
+++ src/main/java/com/Tienda_IIQ/TiendaIiqApplication.java
@@ -0,0 +1,13 @@
+package com.Tienda_IIQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaIiqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaIiqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tienda_IIQ/TiendaIiqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tienda_IIQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaIiqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
